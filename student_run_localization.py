78b9601f827c8c430d9f733c69ce38e60ff33cc1
josetoribio <35315110+josetoribio@users.noreply.github.com>
2023-05-18 14:19:10 -0400

    Update student_run_localization.py

--- student_run_localization.py
+++ student_run_localization.py
@@ -1,60 +1,58 @@
-#!/usr/bin/env python
-"""
-vision_localization_onboard.py
+"""""
+picam_localization_distance
 
-This file can run SLAM or localization on board, offline or online
-"""
+implements Monte-Carlo Localization using the pi-camera
+"""""
 
-
-from student_localization_helper import *
-from cv_bridge import CvBridge, CvBridgeError
-import sys
-import os
-import picamera
-import camera_info_manager
-import rospy
-from geometry_msgs.msg import PoseStamped
+import numpy as np
+import cv2
+from pidrone_pkg.msg import Mode
 from sensor_msgs.msg import Image, Range, CameraInfo
-from analyze_flow import AnalyzeFlow
 from std_msgs.msg import Empty
-from pidrone_pkg.msg import State
-import argparse
+import rospy
 import tf
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import PoseStamped
+from pidrone_pkg.msg import State
+from localization_helper import LocalizationParticleFilter, create_map, PROB_THRESHOLD
+import os
 
 # ---------- map parameters ----------- #
 MAP_PIXEL_WIDTH = 3227  # in pixel
 MAP_PIXEL_HEIGHT = 2447
 MAP_REAL_WIDTH = 1.4  # in meter
 MAP_REAL_HEIGHT = 1.07
+# ------------------------------------- #
 
 # ---------- camera parameters ----------- #
 CAMERA_WIDTH = 320
 CAMERA_HEIGHT = 240
-
-# assume a pixel in x and y has the same length
 METER_TO_PIXEL = (float(MAP_PIXEL_WIDTH) / MAP_REAL_WIDTH + float(MAP_PIXEL_HEIGHT) / MAP_REAL_HEIGHT) / 2.
 CAMERA_CENTER = np.float32([(CAMERA_WIDTH - 1) / 2., (CAMERA_HEIGHT - 1) / 2.]).reshape(-1, 1, 2)
+# ---------------------------------------- #
 
 # ---------- localization parameters ----------- #
 MAX_BAD_COUNT = -10
 NUM_PARTICLE = 30
 NUM_FEATURES = 200
+# ---------------------------------------------- #
 
 
-class Localizer(picamera.array.PiMotionAnalysis):
+class AnalyzePhase:
 
-    def __init__(self, camera, bridge):
-        picamera.array.PiMotionAnalysis.__init__(self, camera)
-        self.bridge = bridge
+    def __init__(self):
+        self.bridge = CvBridge()
         self.br = tf.TransformBroadcaster()
 
-        self.posepub = rospy.Publisher('/pidrone/picamera/pose', PoseStamped, queue_size=1)
-        self.first_image_pub = rospy.Publisher("/pidrone/picamera/first_image", Image, queue_size=1, latch=True)
+        self.posepub = 
+        self.first_image_pub = 
+        #opencv orb  create
+        self.detector = 
+        #use map.jpg to create a map
+        map_grid_kp, map_grid_des = 
 
-        self.detector = cv2.ORB_create(nfeatures=NUM_FEATURES, scoreType=cv2.ORB_FAST_SCORE)
-
-        map_grid_kp, map_grid_des = create_map('map.jpg')
-        self.estimator = LocalizationParticleFilter(map_grid_kp, map_grid_des)
+        #use map grid kp and des inside the localizationparticlefilter
+        self.estimator = 
 
         # [x, y, z, yaw]
         self.pos = [0, 0, 0, 0]
@@ -81,65 +79,70 @@ class Localizer(picamera.array.PiMotionAnalysis):
         self.alpha_yaw = 0.1  # perceived yaw smoothing alpha
         self.hybrid_alpha = 0.3  # blend position with first frame and int
 
-    def write(self, data):
-        curr_img = np.reshape(np.fromstring(data, dtype=np.uint8), (CAMERA_HEIGHT, CAMERA_WIDTH, 3))
-        curr_rostime = rospy.Time.now()
-        self.posemsg.header.stamp = curr_rostime
-        curr_time = curr_rostime.to_sec()
+    #todo image callback
+    def image_callback(self, data):
+       
+        curr_img = 
+        curr_rostime = =
+        self.posemsg.header.stamp = 
+        curr_time = 
 
+        #todo
         # start MCL localization
         if self.locate_position:
-            curr_kp, curr_des = self.detector.detectAndCompute(curr_img, None)
+            curr_kp, curr_des = 
 
             if curr_kp is not None and curr_kp is not None:
                 # generate particles for the first time
                 if self.first_locate:
-                    particle = self.estimator.initialize_particles(NUM_PARTICLE, curr_kp, curr_des)
-                    self.first_locate = False
-                    self.pos = [particle.x(), particle.y(), particle.z(), particle.yaw()]
+                    particle = 
+                    self.first_locate = 
+                    self.pos = 
 
-                    self.posemsg.pose.position.x = particle.x()
-                    self.posemsg.pose.position.y = particle.y()
-                    self.posemsg.pose.position.z = particle.z()
-                    x, y, z, w = tf.transformations.quaternion_from_euler(0, 0, self.pos[3])
+                    self.posemsg.pose.position.x = 
+                    self.posemsg.pose.position.y = 
+                    self.posemsg.pose.position.z = 
+                    x, y, z, w = 
 
-                    self.posemsg.pose.orientation.x = x
-                    self.posemsg.pose.orientation.y = y
-                    self.posemsg.pose.orientation.z = z
-                    self.posemsg.pose.orientation.w = w
+                    self.posemsg.pose.orientation.x = 
+                    self.posemsg.pose.orientation.y = 
+                    self.posemsg.pose.orientation.z = 
+                    self.posemsg.pose.orientation.w = 
 
-                    self.posepub.publish(self.posemsg)
                     print 'first', particle
                 else:
-                    particle = self.estimator.update(self.z, self.angle_x, self.angle_y, self.prev_kp, self.prev_des,
-                                                     curr_kp, curr_des)
+                    particle = 
 
                     # update position
-                    self.pos = [self.hybrid_alpha * particle.x() + (1.0 - self.hybrid_alpha) * self.pos[0],
-                                self.hybrid_alpha * particle.y() + (1.0 - self.hybrid_alpha) * self.pos[1],
-                                self.z,
-                                self.alpha_yaw * particle.yaw() + (1.0 - self.alpha_yaw) * self.pos[3]]
+                    self.pos = 
 
-                    self.posemsg.pose.position.x = self.pos[0]
-                    self.posemsg.pose.position.y = self.pos[1]
-                    self.posemsg.pose.position.z = self.pos[2]
-                    x, y, z, w = tf.transformations.quaternion_from_euler(0, 0, self.pos[3])
 
-                    self.posemsg.pose.orientation.x = x
-                    self.posemsg.pose.orientation.y = y
-                    self.posemsg.pose.orientation.z = z
-                    self.posemsg.pose.orientation.w = w
+                    self.posemsg.pose.position.x = 
+                    self.posemsg.pose.position.y =
+                    self.posemsg.pose.position.z = 
+                    x, y, z, w = 
 
+                    self.posemsg.pose.orientation.x = 
+                    self.posemsg.pose.orientation.y = 
+                    self.posemsg.pose.orientation.z = 
+                    self.posemsg.pose.orientation.w = 
                     self.posepub.publish(self.posemsg)
+
                     print '--pose', self.pos[0], self.pos[1], self.pos[3]
 
                     # if all particles are not good estimations
+                    #todo
                     if is_almost_equal(particle.weight(), PROB_THRESHOLD):
-                        self.map_counter = self.map_counter - 1
+                        #subtract map counter by 1 
+                        self.map_counter = 
                     elif self.map_counter <= 0:
+                        #map counter equals 1 
                         self.map_counter = 1
                     else:
-                        self.map_counter = min(self.map_counter + 1, -MAX_BAD_COUNT)
+                        #use the min() function 
+                        #min()has 2 arguments
+                        #add to map counter and use -MAX_BAD_COUNT
+                        self.map_counter = 
 
                     # if it's been a while without a significant average weight
                     if self.map_counter < MAX_BAD_COUNT:
@@ -165,68 +168,41 @@ class Localizer(picamera.array.PiMotionAnalysis):
 
     def state_callback(self, data):
         """ update z, angle x, and angle y data when /pidrone/state is published to """
-        self.z = data.pose_with_covariance.pose.position.z
-        self.angle_x = data.twist_with_covariance.twist.angular.x
-        self.angle_y = data.twist_with_covariance.twist.angular.y
+       #update with converiance
+        self.z = 
+        self.angle_x = 
+        self.angle_y = 
 
     def reset_callback(self, data):
         """start localization when '/pidrone/reset_transform' is published to (press 'r')"""
         print "Start localization"
-        self.locate_position = True
-        self.first_locate = True
-        self.map_counter = 0
-        self.max_map_counter = 0
+        #SET TO TRUE
+         
+        #SET TO ZERO
+         
 
 
 def is_almost_equal(x, y):
-    epsilon = 1e-8
+    epsilon = 1*10**(-8)
     return abs(x-y) <= epsilon
 
 
 def main():
     node_name = os.path.splitext(os.path.basename(__file__))[0]
     rospy.init_node(node_name)
+    
+    phase_analyzer = AnalyzePhase()
+s
+    phase_analyzer = AnalyzePhase()
+    #subcriber to reset_transform , raspicam_node and pidrone_state 
+    #with the correct callback
+    rospy.Subscriber("")
+    rospy.Subscriber("")
+    rospy.Subscriber("")
 
-    image_pub = rospy.Publisher("/pidrone/picamera/image_raw", Image, queue_size=1, tcp_nodelay=False)
-    camera_info_pub = rospy.Publisher("/pidrone/picamera/camera_info", CameraInfo, queue_size=1, tcp_nodelay=False)
+    print"Start")
 
-    cim = camera_info_manager.CameraInfoManager("picamera", "package://pidrone_pkg/params/picamera.yaml")
-    cim.loadCameraInfo()
-    if not cim.isCalibrated():
-        rospy.logerr("warning, could not find calibration for the camera.")
-
-    try:
-        bridge = CvBridge()
-
-        with picamera.PiCamera(framerate=90) as camera:
-            camera.resolution = (CAMERA_WIDTH, CAMERA_HEIGHT)
-            with AnalyzeFlow(camera) as flow_analyzer:
-                flow_analyzer.setup(camera.resolution)
-                phase_analyzer = Localizer(camera, bridge)
-
-                rospy.Subscriber('/pidrone/reset_transform', Empty, phase_analyzer.reset_callback)
-                rospy.Subscriber('/pidrone/state', State, phase_analyzer.state_callback)
-
-                camera.start_recording("/dev/null", format='h264', splitter_port=1, motion_output=flow_analyzer)
-                print "Starting Flow"
-                camera.start_recording(phase_analyzer, format='bgr', splitter_port=2)
-                last_time = None
-                while not rospy.is_shutdown():
-                    camera.wait_recording(1 / 100.0)
-
-                    if phase_analyzer.prev_img is not None and phase_analyzer.prev_time != last_time:
-                        image_message = bridge.cv2_to_imgmsg(phase_analyzer.prev_img, encoding="bgr8")
-                        image_message.header.stamp = phase_analyzer.prev_rostime
-                        last_time = phase_analyzer.prev_rostime
-                        image_pub.publish(image_message)
-                        camera_info_pub.publish(cim.getCameraInfo())
-
-                camera.stop_recording(splitter_port=1)
-                camera.stop_recording(splitter_port=2)
-        print "Shutdown Received"
-    except Exception:
-        print "Camera Error!!"
-        raise
+    rospy.spin()
 
 
 if __name__ == '__main__':
